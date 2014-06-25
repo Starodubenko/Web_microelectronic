a9f7b62b8cf0c6609aacc2206d58bad70d5b87c7
Rody <Rody10@mail.ru>
2014-06-25 23:26:29 +0600

    Initial commit

---
+++ src/main/java/star/servlet/microelectronic.java
@@ -0,0 +1,16 @@
+package star.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by Rody on 25.06.2014.
+ */
+public class microelectronic extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Web-Microelectronic</title>
+</head>
+<body>
+    <h2>Hello microelectronic world !!!</h2>
+</body>
+</html>
