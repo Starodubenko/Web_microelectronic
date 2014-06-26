dccc1e2b15112322b2d9c013ac63d98adad2eeb9
Rody <Rody10@mail.ru>
2014-06-26 07:31:20 +0600

    Add classes(entyties)

--- src/main/java/star/servlet/MainWeb.java
+++ src/main/java/star/servlet/MainWeb.java
@@ -6,23 +6,31 @@ import microelectronic.factory.MicroelectronicFactory;
 import microelectronic.util.SearchDevice;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 
-/**
- * Created by Rody on 25.06.2014.
- */
-public class microelectronic extends javax.servlet.http.HttpServlet {
+public class MainWeb extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        PrintWriter writer=response.getWriter();
+
         Board board = MicroelectronicFactory.createCompleteBoard(5, Device.Type.RESISTOR, Device.Type.TRANSISTOR, Device.Type.DIOD, Device.Type.CHIP);
         board.setName("FirstBoard");
 
+        writer.println("Element board was created");
+        writer.println(board);
+
         Collections.sort(board.getDevices(), Device.PRICE_COMPARATOR);
 
+        writer.println("Board was sorted by price");
+        writer.println(board);
 
-        SearchDevice.deviceSearch(board.getDevices(),Device.Type.CHIP, Device.ScMaterial.GERMANIUM,10,100);
+
+        writer.println("Found elements: ");
+        writer.println(SearchDevice.deviceSearch(board.getDevices(),Device.Type.CHIP, Device.ScMaterial.GERMANIUM,10,100));
     }
 }
